5cb425a1d1b79599edf7d5a9801d09a54fb0e675
Kaaayur <ekayurov@bk.ru>
2025-03-26 21:19:37 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
+GROUP_CHAT_ID = int(os.environ['GROUP_CHAT_ID'])
+GOOGLE_CREDENTIALS_FILE = os.environ['GOOGLE_CREDENTIALS_FILE']
+SPREADSHEET_KEY = os.environ['SPREADSHEET_KEY']
+
+ANIMATOR_IDS = {
+    "413165965": "Настя",
+    "283779327": "Егор",
+    # ...
+}
+
+KEYWORDS = ["в пути", "на месте", "закончил"]
+
+#  Для PythonAnywhere:
+PA_USERNAME = os.environ['PA_USERNAME']  #  Ваше имя пользователя на PythonAnywhere
